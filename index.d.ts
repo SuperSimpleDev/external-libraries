2378cfa3929be3e6187291eb9dd5fcdcced17771
SuperSimpleDev <simon@supersimple.dev>
2024-12-08 23:06:55 -0500

    Add index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,15 @@
+declare module 'index' {
+  interface Chatbot {
+    addResponses(
+      additionalResponses: Record<
+        string, string | (() => string)
+      >
+    ): void;
+
+    getResponse(message: string): string;
+    getResponseAsync(message: string): Promise<string>;
+  }
+
+  export const Chatbot: Chatbot;
+  export const chatbot: Chatbot;
+}
