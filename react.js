9de66e7ea084709293bd040e56b9d53cc8e6ba71
Spencer Lochne <spencer.lochne@gmail.com>
2025-07-06 19:25:17 -0400

    Add uuid polyfill to react

--- react.js
+++ react.js
@@ -3346,6 +3346,35 @@
 
 })));
 
+// Define the randomUUID() function if it doesn't exist.
+function uuidPolyfill() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (char) {
+    const randomNumber = Math.random() * 16 | 0;
+    const result = char === 'x' ? randomNumber : (randomNumber & 0x3 | 0x8);
+    return result.toString(16);
+  });
+}
+
+// In the browser.
+if (typeof window !== 'undefined') {
+  // Create a fallback if window.crypto is undefined.
+  if (typeof window.crypto === 'undefined') {
+    try {
+      window.crypto = {};
+    } catch (e) {}
+  }
+
+  // Create a fallback if crypto.randomUUID() doesn't exist.
+  if (window.crypto && typeof window.crypto.randomUUID !== 'function') {
+    try {
+      window.crypto.randomUUID = uuidPolyfill;
+      
+      // This is just for testing purposes, to see if the polyfill worked.
+      window.crypto.randomUUID2 = uuidPolyfill;
+    } catch (e) {}
+  }
+}
+
 /*
 MIT License
 
